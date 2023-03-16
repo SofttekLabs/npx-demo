d7ca6b10f5318661840492c99d7b549e12d20e4f
gantonioid <gantonioid@hotmail.com>
2023-03-16 09:52:44 -0600

    prompts username & pwd

--- runnable-demo/src/index.ts
+++ runnable-demo/src/index.ts
@@ -1,5 +1,6 @@
 #! /usr/bin/env node
 import * as _ from 'lodash';
+import * as prompts from 'prompts';
 const yargs = require('yargs/yargs');
 
 const argv = yargs(process.argv.slice(2)).options({
@@ -12,38 +13,66 @@ const flags = argv;
 /**
  * Switch para parsear input y rutear al comando solicitado
  */
-const main = () => {
-    switch (flags.command) {
-        case 'sum':
-            if (flags.args !== "") {
-                try {
-                    console.log(_.sum(JSON.parse(flags.args)));
-                } catch (err) {
-                    console.log("could not parse overArgs, must be an array of numbers");
-                }
-            }
-            break;
-        case 'mult':
-            if (flags.args !== "") {
-                try {
-                    let items = JSON.parse(flags.args);
-                    let res = items[0];
-                    for (let i = 1; i < items.length; i++) {
-                        res *= items[i];
+const main = async () => {
+    const questions: prompts.PromptObject[] = [
+        {
+            type: 'text',
+            name: 'usr',
+            message: 'Enter your username'
+        },
+        {
+            type: 'text',
+            name: 'pwd',
+            message: 'Enter your password'
+        }
+    ];
+
+    const response = await prompts(questions);
+
+    const allowed = response.usr === 'gantonioid' && response.pwd === '123'
+    if (allowed) {
+        switch (flags.command) {
+            case 'sum':
+                if (flags.args !== "") {
+                    try {
+                        console.log(_.sum(JSON.parse(flags.args)));
+                    } catch (err) {
+                        console.log("could not parse overArgs, must be an array of numbers");
                     }
-                    console.log(res);
-                } catch (err) {
-                    console.log("could not parse overArgs, must be an array of numbers");
                 }
-            }
-            break;
-        case "salute":
-            console.log("hello world");
-            break;
-        default:
-            console.error('Unrecognized command');
-            break;
+                break;
+            case 'mult':
+                if (flags.args !== "") {
+                    try {
+                        let items = JSON.parse(flags.args);
+                        let res = items[0];
+                        for (let i = 1; i < items.length; i++) {
+                            res *= items[i];
+                        }
+                        console.log(res);
+                    } catch (err) {
+                        console.log("could not parse overArgs, must be an array of numbers");
+                    }
+                }
+                break;
+            case "salute":
+                console.log("hello world");
+                break;
+            default:
+                console.error('Unrecognized command');
+                break;
+        }
+    } else {
+        console.error('unauthorized')
     }
 }
 
-main();
+main()
+    .then(() => {
+        console.log('done');
+        process.exit(0);
+    })
+    .catch(e => {
+        console.log(e);
+        process.exit(1);
+    })
